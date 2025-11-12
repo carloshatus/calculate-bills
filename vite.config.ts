018b7fd26909060ca1ab44e94b3bbda15ab3d7f0
carloshatus <carlos.hatus@hotmail.com>
2025-11-12 08:02:44 -0300

    feat: config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,45 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}']
+			},
+			manifest: {
+				name: 'Bill Calculator',
+				short_name: 'BillCalc',
+				description: 'A simple bill calculator app.',
+				start_url: '/calculate-bills',
+				display: 'standalone',
+				background_color: '#ffffff',
+				theme_color: '#000000',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				]
+			}
+		})
+	],
 	server: {
 		port: 3000
 	},
